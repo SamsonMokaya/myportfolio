9b976b78f0146f3ef6d559c487b47e7dfdaeb8d3
Samson Mokaya <68181226+SamsonMokaya@users.noreply.github.com>
2023-12-12 17:23:47 +0300

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -67,67 +67,73 @@ export const Frontend_skill = [
 ];
 
 export const Backend_skill = [
-  {
-    skill_name: "Node js",
-    Image: "/node-js.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Express js",
-    Image: "/express.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Mongo db",
-    Image: "/mongodb.png",
-    width: 40,
-    height: 40,
-  },
-  {
-    skill_name: "Fire base",
-    Image: "/Firebase.png",
-    width: 55,
-    height: 55,
-  },
-  {
-    skill_name: "Supa base",
-    Image: "/supabase.png",
-    width: 55,
-    height: 75,
-  },
-  {
-    skill_name: "Postger SQL",
-    Image: "/postger.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "My SQL",
-    Image: "/mysql.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Prisma",
-    Image: "/prisma.webp",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "FastAPI",
-    Image: "/fastapi.png",
-    width: 70,
-    height: 200,
-  },
-  {
-    skill_name: "Codeigniter",
-    Image: "/codeigniter.png",
-    width: 70,
-    height: 70,
-  },
-];
+         {
+           skill_name: "Node js",
+           Image: "/node-js.png",
+           width: 80,
+           height: 80,
+         },
+         {
+           skill_name: "Express js",
+           Image: "/express.png",
+           width: 80,
+           height: 80,
+         },
+         {
+           skill_name: "Mongo db",
+           Image: "/mongodb.png",
+           width: 40,
+           height: 40,
+         },
+         {
+           skill_name: "Redis db",
+           Image: "/redis.png",
+           width: 40,
+           height: 40,
+         },
+         {
+           skill_name: "Fire base",
+           Image: "/Firebase.png",
+           width: 55,
+           height: 55,
+         },
+         {
+           skill_name: "Supa base",
+           Image: "/supabase.png",
+           width: 55,
+           height: 75,
+         },
+         {
+           skill_name: "Postger SQL",
+           Image: "/postger.png",
+           width: 70,
+           height: 70,
+         },
+         {
+           skill_name: "My SQL",
+           Image: "/mysql.png",
+           width: 70,
+           height: 70,
+         },
+         {
+           skill_name: "Prisma",
+           Image: "/prisma.webp",
+           width: 70,
+           height: 70,
+         },
+         {
+           skill_name: "FastAPI",
+           Image: "/fastapi.png",
+           width: 70,
+           height: 200,
+         },
+         {
+           skill_name: "Codeigniter",
+           Image: "/codeigniter.png",
+           width: 70,
+           height: 70,
+         },
+       ];
 
 export const Full_stack = [
   {
